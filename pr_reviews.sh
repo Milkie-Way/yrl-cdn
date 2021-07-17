1a63e06c7c28fc347198de4db6cb99ddb7b496e4
Sudeep C <58381775+sudcha2@users.noreply.github.com>
2021-07-16 21:07:37 -0700

    updates

--- pr_reviews.sh
+++ pr_reviews.sh
@@ -1,5 +1,4 @@
-gh pr create -R https://github.com/Milkie-Way/yrl-cdn.git -H dev -B staging --title "staging Release" --body "Staging Release"
-gh pr create -R https://github.com/Milkie-Way/yrl-cdn.git -H staging -B prod --title "prod Release" --body "prod Release"
+gh pr create -R https://github.com/Milkie-Way/yrl-cdn.git -H dev -B prod --title "prod Release" --body "prod Release"
 
 
 gh pr merge https://github.com/Milkie-Way/yrl-lp/pull/99 -m --delete-branch=false
