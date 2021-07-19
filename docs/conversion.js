770ebfc0f23680cb50444f05cf618851ecc9f6bd
Sudeep C <58381775+sudcha2@users.noreply.github.com>
2021-07-18 21:09:03 -0700

    updates

--- docs/conversion.js
+++ docs/conversion.js
@@ -4,7 +4,8 @@ const setCookie = () => {
   const y = urlParams.get("yrl_y");
   const d = new Date().getTime() + 1000 * 60 * 60 * 24 * 7;
   const expires = "expires=" + new Date(d).toUTCString();
-  if (y) document.cookie = `__yrl_referral=${y};` + expires;
+  const parentDomain = window.location.hostname;
+  if (y) document.cookie = `yrl_cnv_ref=${y};domain=${parentDomain};` + expires;
 };
 
 setCookie();
@@ -16,8 +17,8 @@ const yrl_conversion = (content = {}) => {
     while (c.charAt(0) == " ") {
       c = c.substring(1);
     }
-    if (c.indexOf("__yrl_referral=") === 0) {
-      refId = c.substring("__yrl_referral=".length, c.length);
+    if (c.indexOf("yrl_cnv_ref=") === 0) {
+      refId = c.substring("yrl_cnv_ref=".length, c.length);
     }
   }
 
@@ -32,7 +33,7 @@ const yrl_conversion = (content = {}) => {
         body: JSON.stringify({ yrl: refId, content: content }),
       });
       const resp = await rawResponse.json();
-      document.cookie = `__yrl_referral=; Expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
+      document.cookie = `yrl_cnv_ref=; Expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
       resolve("success");
     } catch (err) {
       console.log("something went wrong: ", err);
